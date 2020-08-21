046eab7c9dd33bd3b02437b4605fbb4ed570557b
Variellka <50990037+Variellka@users.noreply.github.com>
2020-08-21 18:16:51 +0300

    Add files via upload

---
+++ todo-list/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import "./App.css";
+
+function App() {
+    const [currentTodo, setCurrentTodo] = useState("");
+    const [todos, setTodos] = useState([]);
+    const handleClick = (e) => {
+        createNewTodo(currentTodo);
+        setCurrentTodo("");
+    };
+    const createNewTodo = (currentTodo) => {
+        let todosArray = [...todos];
+        todosArray.push({todo: currentTodo});
+        setTodos(todosArray);
+    }
+    const toggleClass = (event) => {
+        event.target.classList.toggle('checked')
+    }
+    const deleteTodo = (index) => {
+        let todosArray = [...todos];
+        todosArray.splice(index, 1);
+        setTodos(todosArray);
+    }
+    return (
+        <div>
+            <input value={currentTodo} onChange={e => {setCurrentTodo(e.target.value);}}/>
+            <button onClick={handleClick}>Set</button>
+            {todos.map((todo, index) => (
+                <div className = 'todoBlock'>
+                    <div key={todo} onClick={toggleClass}>{todo.todo}</div>
+                    <button onClick={() => deleteTodo(index)}>Delete</button>
+                </div>
+            ))}
+        </div>
+    );
+}
+export default App;
